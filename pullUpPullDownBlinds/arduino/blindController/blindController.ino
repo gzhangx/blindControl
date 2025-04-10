a4745ab43036cb2f3e587b9b03a220afebc7f57c
Gang Zhang <gzhangx@hotmail.com>
2025-04-09 20:06:01 -0400

    control

--- pullUpPullDownBlinds/arduino/blindController/blindController.ino
+++ pullUpPullDownBlinds/arduino/blindController/blindController.ino
@@ -7,6 +7,12 @@
 #define SLAVE_ADDRESS 0x08
 
 
+#define NUM_WIRES 1
+byte wires[1][2] = {
+  {2,3},
+};
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(19200);
@@ -15,7 +21,7 @@ void setup() {
     ; // Do nothing until serial connection is established
   }
   // Print a message when setup is complete
-  Serial.println("Serial communication started!");
+  Serial.println("Serial communication started! i2c on " + String(SLAVE_ADDRESS));
   // initialize digital pin LED_BUILTIN as an output.  
   Wire.begin(SLAVE_ADDRESS);
 
@@ -23,39 +29,76 @@ void setup() {
   Wire.onRequest(sendData);
   
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-}
-
-byte wires[1][2] = {
-  {8,9},
-};
-
-int ledAccu = 0;
-byte ledState = LOW;
-
-char cmd= ' ';
-byte who = 0;
-unsigned long turnTime = 0;
-
-// the loop function runs over and over again forever
-void loop() {
-  ledAccu++;
-  if (ledAccu>20000) {
-    ledAccu = 0;
-    digitalWrite(LED_BUILTIN, ledState);
-    if (ledState == LOW) ledState = HIGH; else ledState = LOW;
+  //pinMode(2, INPUT);
+  //pinMode(8, OUTPUT);
+  //pinMode(9, OUTPUT);
+  for (int i = 0; i < NUM_WIRES; i++) {
+     for (int j = 0; j < 2; j++) {
+      pinMode(wires[i][j], OUTPUT);
+      digitalWrite(wires[i][j], LOW);
+     }
   }
 
+  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+}
+
+
+long ledAccu = 0;
+byte ledState = LOW;
+
+char cmd= ' '; //shoudl be ' '
+byte who = 0;
+unsigned long turnTime = 0; //should be 0
+
+unsigned long lastChangeTime = 0;
+int loopCount = 0;
+// the loop function runs over and over again forever
+int D12 = 0, D11=0;
+int RUNNING = 0;
+void loop() {
+
+unsigned long timeFromLastChange = millis() - lastChangeTime;
+  
+  loopCount++;  
+  ledAccu++;
+  if (ledAccu>600000) {
+    ledAccu = 0;
+    digitalWrite(LED_BUILTIN, ledState);
+    if (ledState == LOW) ledState = HIGH; else ledState = LOW;      
+  }
+
+  //digitalWrite(8, HIGH);
+  //digitalWrite(9, HIGH);
   if (cmd == 'c' || cmd == 'C') {
     if (turnTime < millis()) {
       cmd = ' ';
       digitalWrite(wires[who][0], LOW);
       digitalWrite(wires[who][1], LOW);
-      Serial.print("Stopping");
+      Serial.println("Stopping");
     }
   }
-    
+
+
+   D12 = digitalRead(12);
+   D11 = digitalRead(11); 
+   if (D12 == LOW) {
+      Serial.println("LOW");
+      digitalWrite(wires[who][0], HIGH);
+      digitalWrite(wires[who][1], HIGH);
+      RUNNING = 1;
+   } if (D11 == LOW){
+    digitalWrite(wires[who][0], HIGH);
+      digitalWrite(wires[who][1], LOW);
+      RUNNING = 1;
+   } else {
+    if (RUNNING) {
+    Serial.println("stoop");
+    }
+      digitalWrite(wires[who][0], LOW);
+      digitalWrite(wires[who][1], LOW);
+      RUNNING = 0;
+   }
 }
 
 
@@ -79,8 +122,8 @@ void receiveData(int bytecount)
     w1 = LOW;
     w2 = LOW;
   }
-  digitalWrite(wires[who][0], w1);
-  digitalWrite(wires[who][1], w2);
+  digitalWrite(wires[who][0], w1);  //ENABLE
+  digitalWrite(wires[who][1], w2);  //DIR
   turnTime = 0;
   while (Wire.available()) {
      // Shift existing result left 8 bits and add new byte
@@ -92,5 +135,6 @@ void receiveData(int bytecount)
 
 void sendData()
 {
-  //Wire.write(data_to_echo);
+  Serial.println("sending data");
+  Wire.write("123456",3);
 }
